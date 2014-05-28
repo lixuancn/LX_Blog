94c58dfe9e50c7fc2ad60b47edd217251db6693c
lixuancn <lixuan868686@163.com>
2014-05-28 17:51:08 +0800

    修复错误提示页面的BUG

--- templates/show_message.tpl.php
+++ templates/show_message.tpl.php
@@ -1,6 +1,33 @@
-﻿<?php
-include 'front/header.tpl.php';
-?>
+﻿<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title><?php echo isset($seo_title) ? $seo_title : SEO_TITLE; ?> - <?php echo WEB_NAME?></title>
+    <meta name="keywords" content="<?php echo isset($seo_keywords) ? $seo_keywords : SEO_KEYWORDS;?>">
+    <meta name="description" content="<?php echo isset($seo_description) ? $seo_description : SEO_DESCRIPTION;?>">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+
+
+    <!-- 引入BootStrap的CSS文件 -->
+    <link rel="stylesheet" href="<?php echo CSS_DIR?>bootstrap.min.css">
+    <!-- 引入BootStrap的JS文件 -->
+    <script src="<?php echo JS_DIR?>bootstrap.min.js"></script>
+    <!-- 引入JQuery文件 -->
+    <script src="<?php echo JS_DIR?>jquery.min.js"></script>
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="<?php echo JS_DIR?>html5shiv.min.js"></script>
+    <script src="<?php echo JS_DIR?>respond.min.js"></script>
+    <![endif]-->
+
+
+    <link href="<?php echo CSS_DIR?>style.css" rel="stylesheet">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="<?php echo IMAGE_DIR?>favicon.ico" rel="shortcut icon">
+</head>
+<body>
 <title>提示信息</title>
 <meta http-equiv="cache-control" content="no-cache" />
 <div style="height:12px;"></div>
--- wwwroot/templates/show_message.tpl.php
+++ wwwroot/templates/show_message.tpl.php
@@ -1,6 +1,26 @@
-﻿<?php
-include 'admin/header.tpl.php';
-?>
+﻿<!DOCTYPE html>
+<html>
+<head>
+    <title>管理后台 - <?php echo WEB_NAME?></title>
+    <meta name="keywords" content="管理后台">
+    <meta name="description" content="管理后台">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="<?php echo ADMIN_IMAGE_DIR?>favicon.ico" rel="shortcut icon">
+    <!-- 引入JQuery文件 -->
+    <script src="<?php echo ADMIN_JS_DIR?>jquery.min.js"></script>
+    <!-- 引入BootStrap的CSS文件 -->
+    <link href="<?php echo ADMIN_CSS_DIR?>bootstrap.min.css" rel="stylesheet">
+    <!-- 引入BootStrap的JS文件 -->
+    <script src="<?php echo ADMIN_JS_DIR?>bootstrap.min.js"></script>
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="<?php echo ADMIN_JS_DIR?>html5shiv.min.js"></script>
+    <script src="<?php echo ADMIN_JS_DIR?>respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
 <title>提示信息</title>
 <meta http-equiv="cache-control" content="no-cache" />
 <div style="height:12px;"></div>
