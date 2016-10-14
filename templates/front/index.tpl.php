b2127cef2cff1d1291f4be7eba230d9e834bcc87
lixuancn <lixuan868686@163.com>
2016-10-14 10:30:11 +0800

    首页采用独立样式

--- templates/front/index.tpl.php
+++ templates/front/index.tpl.php
@@ -1,6 +1,68 @@
 <?php
-include 'header.tpl.php';
+if (!defined('ENTRY_NAME')) exit("Not Allowed to request this file!");
 ?>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <title>PHP博客_PHP教程_PHP分享 - Lane Blog</title>
+        <meta name="keywords" content="PHP博客,PHP教程,PHP分享">
+        <meta name="description" content="欢迎来到Lane的PHP博客。这是关于PHP博客，有大量的PHP教程和分享，以及PHP资源下载。根据PHP博客作者亲身经历所做的PHP教程。">
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <!-- 引入BootStrap的CSS文件 -->
+        <link rel="stylesheet" href="<?php echo CSS_DIR?>bootstrap.min.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="<?php echo IMAGE_DIR?>favicon.ico" rel="shortcut icon">
+    </head>
+
+<body>
+
+
+<header class="navbar navbar-inverse navbar-fixed-top bs-docs-nav" role="banner">
+    <div class="container">
+        <div class="navbar-header">
+            <button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".bs-navbar-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a href="<?php echo GAME_URL?>" class="navbar-brand" title="Lane PHP Blog">Lane</a>
+        </div>
+        <nav class="collapse navbar-collapse bs-navbar-collapse" role="navigation">
+            <ul class="nav navbar-nav">
+                <li <?php if($actionMenuId == 0){echo ' class="active"';}?>>
+                    <a href="<?php echo GAME_URL?>" title="李轩PHP博客">首页</a>
+                </li>
+                <?php foreach($menuList as $menu){ ?>
+                    <li <?php if($actionMenuId == $menu['id']){echo ' class="active"';}?>>
+                        <a href="<?php echo GAME_URL?>menu/main/mid-<?php echo $menu['id'];?>"><?php echo $menu['name'];?></a>
+                    </li>
+                <?php } ?>
+                <li class="divider-vertical"></li>
+            </ul>
+            <form class="navbar-form navbar-left" role="form" action="<?php echo GAME_URL?>search/main/" method="get">
+                <div class="form-group">
+                    <label class="sr-only" for="exampleInputEmail2">Search PHP</label>
+                    <input type="text" class="form-control" name="keywords" placeholder="Search" value="<?php if(isset($keywords)){echo $keywords;}?>">
+                </div>
+                <button type="submit" class="btn btn-primary">搜索</button>
+            </form>
+        </nav>
+    </div>
+</header>
+
+
+<div class="container">
+    <div class="jumbotron">
+        <h1 class="text-center">Lane<small>Blog</small></h1>
+        <p class="text-center">
+            <small>蝼蚁虽小，也有梦想</small>
+        </p>
+        <p class="text-center">
+            <small><a href="http://meepops.lanecn.com/">PHP Socket服务</a>  |  <a href="http://lanewechat.lanecn.com/">PHP微信开发框架</a>  |  <a href="http://www.lanecn.com/article/main/aid-11">开源博客</a></small>
+        </p>
+    </div>
+</div>
 <div class="container">
     <div class="row">
         <div class="col-xs-12 col-sm-12 col-md-8">
@@ -94,6 +156,27 @@ include 'header.tpl.php';
         </div>
     </div>
 </div>
-<?php
-include 'footer.tpl.php';
-?>
+
+
+<div class="container">
+    <footer class="footer">
+        <p class="pull-right"><a href="#" rel="nofollow">回到顶端</a></p>
+    </footer>
+    <div class="row">
+        <div class="text-center">©2014 <a href="http://www.lanecn.com">www.lanecn.com</a> , All rights reserved. Power By <a href="http://www.lanecn.com">Li Xuan</a>.&nbsp;&nbsp;<a rel="nofollow" href="http://www.miitbeian.gov.cn/">京ICP备14005030号</a> <script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_5824445'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s23.cnzz.com/stat.php%3Fid%3D5824445' type='text/javascript'%3E%3C/script%3E"));</script></div>
+    </div>
+</div>
+</body>
+</html>
+<!-- 引入JQuery文件 -->
+<script src="<?php echo JS_DIR?>jquery.min.js"></script>
+<!-- 引入BootStrap的JS文件 -->
+<script src="<?php echo JS_DIR?>bootstrap.min.js"></script>
+<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+<script src="<?php echo JS_DIR?>html5shiv.min.js"></script>
+<script src="<?php echo JS_DIR?>respond.min.js"></script>
+<![endif]-->
+
+<link href="<?php echo CSS_DIR?>style.css" rel="stylesheet">
